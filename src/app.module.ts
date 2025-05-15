a599bbb7d0b1e431fcf257cbeb9eeeb43aef8b20
gabrielteodoroo <biel.teodorob@gmail.com>
2025-05-15 14:26:51 -0300

    feat: implement authentication module with JWT strategy and guards

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UserModule,
+    AuthModule
+  ],
 })
-export class AppModule {}
+export class AppModule { }
