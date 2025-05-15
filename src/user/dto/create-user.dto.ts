b799378cc6a9ff2fa61f7e5bea37085fd9b66279
gabrielteodoroo <biel.teodorob@gmail.com>
2025-05-15 11:55:08 -0300

    fix: ensure validation messages are included for required fields in CreateUserDto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,13 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
+
 export class CreateUserDto {
+  @IsNotEmpty({ message: "Name is required" })
   name: string;
+
+  @IsEmail()
+  @IsNotEmpty({ message: "Email is required" })
   email: string;
+
+  @IsNotEmpty({ message: "Password is required" })
   password: string;
 }
