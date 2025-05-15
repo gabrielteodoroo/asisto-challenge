74bb91fd8855290a9caefa9f67acb0b3ecfe5b8f
gabrielteodoroo <biel.teodorob@gmail.com>
2025-05-15 11:55:16 -0300

    fix: correct method signature for createUser in UserRepository interface

--- src/user/repository/user-repository.interface.ts
+++ src/user/repository/user-repository.interface.ts
@@ -1,5 +1,9 @@
 import { User } from "../entities/User";
 
 export abstract class UserRepository {
-  abstract createrUser(user: User): Promise<void>;
+  abstract createUser(user: User): Promise<User>;
+  abstract findByEmail(email: string): Promise<User | null>;
+  abstract findById(id: string): Promise<User | null>;
+  abstract save(user: User): Promise<void>;
+  abstract findMany(): Promise<User[]>;
 }
